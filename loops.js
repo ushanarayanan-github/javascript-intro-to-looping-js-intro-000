ab2e03dbaa67d666b2cff7f4f6c3eec62b7c74eb
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-06 04:40:18 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,15 +20,3 @@ function doWhileLoop(n){
 }
 
 
-let i=0
-
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
-
-function doWhileLoop(num) {
-do {
-  console.log("I run once regardless.");
-} while (incrementVariable() < num)
-}
